7b60221949a9b4a719e1a8b6799456b7b12887d0
gilvansimoes <gilvan.simoesdebarros@gmail.com>
2025-03-26 00:20:48 -0300

    Alteracao Responsividade

--- js/main.js
+++ js/main.js
@@ -409,8 +409,8 @@ function sendEmail(ptitulo, pEmail, pNome, pWhatsapp, pMessage, pSubmit) {
           backgroundSize: "contain",
           backgroundRepeat: "no-repeat",
           backgroundImage: "url(./assets/gmail.svg)",
-          bottom: "11.5%",
-          right: "57%",
+          bottom: "3.5%",
+          right: "55%",
           zIndex: "10",
           borderRadius: "0",
           boxShadow: "none",
@@ -429,22 +429,3 @@ function sendEmail(ptitulo, pEmail, pNome, pWhatsapp, pMessage, pSubmit) {
       }
     });
 }
-function loading() {
-    const loading = document.getElementById('loading');
-    const allContent = document.getElementById('allContent');
-    allContent.style.display = 'none';
-    loading.style.display = 'flex';
-    loading.innerHTML += `
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" width="200" height="200" style="shape-rendering: auto; display: block; background: transparent;" xmlns:xlink="http://www.w3.org/1999/xlink">
-        <g>
-            <path style="transform:scale(0.8);transform-origin:50px 50px" stroke-linecap="round" d="M24.3 30C11.4 30 5 43.3 5 50s6.4 20 19.3 20c19.3 0 32.1-40 51.4-40 C88.6 30 95 43.3 95 50s-6.4 20-19.3 20C56.4 70 43.6 30 24.3 30z" stroke-dasharray="42.76482137044271 42.76482137044271" stroke-width="8" stroke="#3f7a49" fill="none">
-                <animate values="0;256.58892822265625" keyTimes="0;1" dur="1s" repeatCount="indefinite" attributeName="stroke-dashoffset"></animate>
-            </path>
-        </g>
-    </svg>
-    `;
-    setTimeout(() => {
-        loading.style.display = 'none';
-        allContent.style.display = 'block';
-    }, 650);
- }
